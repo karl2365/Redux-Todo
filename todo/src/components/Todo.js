010fa6bee2db8f364189cc85907562d07cfcd06f
Karl Beksa <karlbeksa@gmail.com>
2019-06-18 17:08:02 -0400

    MVP complete - no styling

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { addNewTodo, toggleTodo } from '../actions';
 
 
 class Todo extends React.Component {
+    state = {
+        newTodo: ''
+    }
 
 
+    handleChanges = e => {
+        this.setState({ newTodo: e.target.value });
+    };
+
+    addTodo = e => {
+        e.preventDefault();
+        this.props.addNewTodo(this.state.newTodo);
+        this.setState({ newTodo: '' });
+    };
+
+    toggleTodo = (e, index) => {
+        e.preventDefault();
+        this.props.toggleTodo(index);
+    };
 
     render(){
         
         return(
             <div>
-                {this.props.todos.map(todo => (
-                    <p>{todo}</p>
 
+                {this.props.todos.map((todo, index) => (
+                    <p onClick={e => this.toggleTodo(e, index)} key={index}>{todo.value}&nbsp;&nbsp;{todo.completed && <strong>Completed</strong>}</p>
                 ))}
+                <input type='text' value={this.state.newTodo} onChange={this.handleChanges} placeholder='New Todo' />
+                <button onClick={this.addTodo}>Add Todo</button>
 
             </div>
         )
-
-
     }
 }
 
@@ -30,7 +48,4 @@ const mapStateToProps = state => {
 }
 
 
-
-
-
-export default connect(mapStateToProps,{})(Todo);
+export default connect(mapStateToProps,{addNewTodo, toggleTodo})(Todo);
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,10 +1,39 @@
+import { ADD_NEW_TODO, TOGGLE_TODO } from "../actions";
+
 const initialState = {
-    todos: ['Clean', 'Code Redux']
+    todos: [
+        {value:'Go to Store',
+        completed: false},
+        {value: 'Code Redux', 
+        completed: false},
+        {value:'Eat',
+        completed: false},
+        {value: 'Sleep',
+        completed: false}]
 }
 
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
-
+        case ADD_NEW_TODO:
+            const newTodo = { value: action.payload, completed: false };
+            return {
+                ...state,
+                todos: [...state.todos, newTodo]
+            }
+        case TOGGLE_TODO:
+            return {
+                ...state,
+                todos: state.todos.map((todo, index) => {
+                    if (action.payload === index) {
+                        return{
+                            ...todo,
+                            completed: !todo.completed
+                        }
+                    }else{
+                        return todo;
+                    }
+                })
+            }
         default:
             return state;
     }
